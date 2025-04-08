acb93052c3e1ce7a84ab9775d182354ecfda5830
Pedro Verani <pedropverani@hotmail.com>
2025-04-08 17:51:27 -0300

    making it run

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-console.log('--- APP.TS IS EXECUTING ---');
+console.log('--- APP.TS IS EXECsUTING ---');
 import HyperExpress from 'hyper-express';
 
 const WebServer = new HyperExpress.Server(); 
@@ -11,7 +11,7 @@ WebServer.get('/', (request: any, response: any) => {
 });
 
 WebServer.listen(3001)
-    .then((socket: any) => console.log(`✅ WebServer listening on port 3001`))
+    .then((socket: any) => console.log(`✅ WebServer listening on port 444`))
     .catch((error: any) => console.error('❌ Failed to start WebServer:', error)); 
 
-console.log('Attempting to start listener...'); 
+console.log('Attempting to start...'); 
